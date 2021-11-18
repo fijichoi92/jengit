e99fb60c44dcd41dc73ce9f054375d758ca03331
khchoi <fijichoi92@gmail.com>
2021-11-18 16:26:03 +0900

    test5

--- dock.sh
+++ dock.sh
@@ -2,7 +2,9 @@
 amazon-linux-extras install -y docker
 systemctl start docker
 docker pull jenkins/jenkins:lts
-rmdir -rf /jenkins
-mkdir /jenkins
-docker run -itd -p 60800:8080 -v /jenkins:/var/jenkins_home --name j1 jenkins/jenkins:lts
+rm -rf /jen
+mkdir /jen
+chmod 777 /jen
+docker rm -f j1
+docker run -itd -p 60800:8080 -v /jen:/var/jenkins_home --name j1 jenkins/jenkins:lts
 docker exec j1 cat /var/jenkins_home/secrets/initialAdminPassword
