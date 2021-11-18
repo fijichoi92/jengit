fedf9b6f95e6c3ae5832dfc2c2a0fab130b25022
khchoi <fijichoi92@gmail.com>
2021-11-18 15:50:41 +0900

    test4

--- dock.sh
+++ dock.sh
@@ -5,4 +5,4 @@ docker pull jenkins/jenkins:lts
 mkdir /jenkins
 chmod 777 /jenkins
 docker run -itd -p 60800:8080 -v /jenkins:/var/jenkins_home --name j1 jenkins/jenkins:lts
-docker exec j1 cat /var/jenkins_home/secrets/initialAdminPassword
+docker exec j1 cat /jenkins/secrets/initialAdminPassword
